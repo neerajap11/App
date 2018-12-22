50c57bcfa9fbaf703c76f3f44f813e691d48293d
NEERAJA P <neerups11@gmail.com>
2018-12-22 19:14:59 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+/**
+ * React Native Drawer Application
+ * https://github.com/taimoormk/react-native-drawer-app
+ * Author: Taimoor Khan
+ * Email: taimoor.m.k@live.com
+ */
+
+// import dependencies here
+import React, { Component } from 'react';
+import { App } from './src/config/router';
+
+
+
+export default class App1 extends Component {
+  render() {
+    return (
+      <App />
+    );
+  }
+}
